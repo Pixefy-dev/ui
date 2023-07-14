ae7e0eb973e531cb90421e2650443b72b52f43ff
Artyom Petrunin <artem.petrunin@list.ru>
2023-07-15 00:13:05 +0300

    support item ready

---
+++ src/components/General/SupportItem/index.stories.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { Meta, Story } from "@storybook/react";
+
+import AvatarIcon from "@/assets/icons/avatar.png";
+
+import { SupportItem, SupportItemProps } from "./index";
+
+const meta: Meta = {
+  title: "Pixefy UI/General/SupportItem",
+  component: SupportItem,
+  argTypes: {
+    type: {
+      options: ["outgoing", "incoming"],
+      control: { type: "radio" },
+      defaultValue: "outgoing"
+    },
+    avatarUrl: {
+      defaultValue: AvatarIcon,
+      control: {
+        type: "file",
+        accept: ".png"
+      },
+      table: {
+        defaultValue: { summary: AvatarIcon }
+      }
+    },
+    nickname: {
+      defaultValue: "Тестовый"
+    },
+    time: {
+      defaultValue: "Тестовая минута назад"
+    },
+    message: {
+      defaultValue: "Тестовый тестик тест"
+    }
+  },
+  parameters: {
+    backgrounds: { default: "dark" },
+    controls: { expanded: true }
+  }
+};
+
+export default meta;
+
+const Template: Story<SupportItemProps> = args => <SupportItem {...args} />;
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default = Template.bind({});
+
+export const Outgoing = Template.bind({});
+
+Outgoing.args = {
+  type: "outgoing",
+  nickname: "Поддержка",
+  time: "30 минут назад",
+  message: "Привет, дорогой игрок! Здесь ты можешь задать любой вопроc связанный с игровым процессом и не только!"
+};
+export const Incoming = Template.bind({});
+
+Incoming.args = {
+  type: "incoming",
+  nickname: "Поддержка",
+  time: "30 минут назад",
+  message: "Привет, дорогой игрок! Здесь ты можешь задать любой вопроc связанный с игровым процессом и не только!"
+};
---
+++ src/components/General/SupportItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import classNames from "@/functions/classNames";
+
+import "./index.scss";
+
+export interface SupportItemProps {
+  type: "outgoing" | "incoming"
+  avatarUrl: string
+  nickname: string
+  time: string
+  message: string
+}
+
+export function SupportItem(props: SupportItemProps) {
+  const {
+    type = "outgoing",
+    avatarUrl,
+    nickname,
+    time,
+    message
+  } = props;
+
+  return (
+    <div className={classNames("supportItem", `supportItemType_${type}`)}>
+      <div className="supportItem__line" />
+      <div className="supportItem__content">
+        <div className="supportItem__info">
+          <img
+            className="supportItem__avatar"
+            src={avatarUrl}
+            alt=""
+          />
+          <div className="supportItem__userInfo">
+            <div className="supportItem__nickname">{nickname}</div>
+            <div className="supportItem__time">{time}</div>
+          </div>
+        </div>
+        <div className="supportItem__message">{message}</div>
+      </div>
+    </div>
+  );
+}
