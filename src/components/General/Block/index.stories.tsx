41de01fe8bff749bb6744a24f725c3fa6aa60fae
Artyom Petrunin <26293210+SkyGopnik@users.noreply.github.com>
2023-08-11 12:02:59 +0300

    some fixes

--- src/components/General/Block/index.stories.tsx
+++ src/components/General/Block/index.stories.tsx
@@ -5,15 +5,7 @@ import { Block } from "./index";
 
 const meta: Meta = {
   title: "Pixefy UI/General/Block",
-  component: Block,
-  argTypes: {
-    children: {
-      defaultValue: "Default",
-      control: {
-        type: "text"
-      }
-    }
-  }
+  component: Block
 };
 
 export default meta;
@@ -23,3 +15,8 @@ const Template: StoryFn = args => <Block {...args} />;
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
+
+Default.args = {
+  type: "dark",
+  children: "Default"
+};
